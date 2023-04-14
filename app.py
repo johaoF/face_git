bd8f897ed7ff8bace2be1bd9a53eb883e14f6d82
johaoF <123512952+johaoF@users.noreply.github.com>
2023-04-13 23:58:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -49,23 +49,3 @@ def webhook():
                     pass
 
     return "ok", 200
-
-def send_message(recipient_id, message_text):
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    data = json.dumps({
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "text": message_text
-        }
-    })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-
