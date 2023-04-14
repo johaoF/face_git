93758dc0cd63ae28e8bb3413b72a4f325c66258d
johaoF <123512952+johaoF@users.noreply.github.com>
2023-04-13 23:10:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+        if not request.args.get("hub.verify_token") == os.environ["FB_VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
@@ -59,7 +59,7 @@ def send_message(recipient_id, message_text):
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+        "access_token": os.environ["FB_PAGE_ACCESS_TOKEN"]
     }
     headers = {
         "Content-Type": "application/json"
