28b449b3ec254156ae29d24c3f45422027a7e3ab
johaoF <123512952+johaoF@users.noreply.github.com>
2023-04-13 23:49:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,6 @@ def webhook():
     # endpoint for processing incoming messaging events
 
     data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
-
     if data["object"] == "page":
 
         for entry in data["entry"]:
@@ -51,4 +49,23 @@ def webhook():
                     pass
 
     return "ok", 200
+
+def send_message(recipient_id, message_text):
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
 
