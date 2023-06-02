9d1dabde24f0786c0c09ad35c46a0a15462f36c4
johaoF <123512952+johaoF@users.noreply.github.com>
2023-06-02 01:03:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,20 @@ def webhook():
                                 text_response.append(paragraph)
                             seen_paragraphs.add(paragraph)
                     response_text = "\n".join(text_response)
-                               
+                    if response.query_result.intent.display_name == 'Reservaciones':
+                       fecha_reservacion = response.query_result.parameters.fields['fecha'].string_value
+                          # Guardar la fecha de reservación en la base de datos
+                       if collection:
+                          documento = {'sender_id': sender_id, 'fecha_reservacion': fecha_reservacion}
+                          collection.insert_one(documento)
+                          print('Fecha de reservación guardada en la base de datos')
+                       else:
+                          print('Error al conectar a la base de datos')
+
+# Enviar la respuesta al usuario a través de Facebook Messenger
+if sender_id is not None:
+    send_message(sender_id, response_text)
+
                 if sender_id is not None:
                     send_message(sender_id, response_text)
 
